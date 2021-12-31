c5c14ac139b0340102a116ed054d5f3d885425c8
Justin Focus <justin.focus@erasys.de>
2021-12-31 13:27:23 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+   
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    name="stylegan-ada-hidden-vortices",
+    py_modules=["dnnlib", "ffhq_dataset", "metrics", "training", "utils"],
+    version="1.0",
+    description="",
+    author="Luke Nickel & Justin Focus",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    include_package_data=True,
+    extras_require={'dev': ['pytest']},
+)
